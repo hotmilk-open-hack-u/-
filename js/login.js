dbfa42f1592031e1d3bdd36577bb485a2a5c634d
UekusaTomoki <tmkueks19@gmail.com>
2015-11-01 23:29:55 +0900

    Implementation form validation

--- js/login.js
+++ js/login.js
@@ -1,27 +1,57 @@
 $(document).ready(function(){
-  $("#login").submit(function(){
-      var mail = $("#mail").val();
-      var password = $("#password").val();
-      console.log("mail:\t"+mail+"\npass:\t"+password);
-    //   $.ajax({
-    //       type: 'post',
-    //       url: 'http://210.140.71.3/login',
-    //       data: {"user":{"email":"hoge@gmail.com","password":"hogehoge"}},
-    //       dataType: "json",
-    //       crossDomain : true,
-    //       success: function(data){
-    //           console.log("success");
-    //           console.log(data);
-    //       },
-    //       error : function(data) {
-    //           console.log("error");
-    //           console.log(data);
-    //       },
-    //       complete : function(data){
-    //           console.log("complete");
-    //           console.log(data);
-    //       }
-    //   });
-      return false;
-  });
+    // validation機能の適応
+    $("#login").validate({
+        rules : {
+            mailaddress: {
+                required: true
+            },
+            password: {
+                required:true
+            }
+        },
+        messages: {
+            mailaddress: {
+                required: "メールアドレスが入力されていません"
+            },
+            password:{
+                required: "パスワードが入力されていません"
+            }
+        },
+        errorClass:"error-text"
+    });
+
+    // ログインボタンのクリック時
+    $("#login").submit(function(){
+        // メールアドレスとパスワードの取得
+        var mail = $("#mail").val();
+        var password = $("#password").val();
+        console.log("mail:\t"+mail+"\npass:\t"+password);
+
+        $.ajax({
+            type: 'post',
+            url: 'http://210.140.71.3/login',
+            data: {"user":{"email":"hoge@gmail.com","password":"hogehoge"}},
+            dataType: "json",
+            success: function(data){
+                console.log("success");
+                console.log(data);
+
+                if(data.id == null || data.authentication_token == null){
+                    console.log("not login");
+                }else{
+                    console.log("login");
+                    // データ保存
+                    localStorage.setItem("user_id",data.id);
+                    localStorage.setItem("token",data.authentication_token);
+                    // ホームページに移動
+                    // window.location.href = 'home.html';
+                }
+            },
+            error : function(data) {
+                console.log("error");
+                console.log(data);
+            }
+        });
+        return false;
+    });
 });
