549c5287dff8dc64da0e286818f055bb5f237889
UekusaTomoki <tmkueks19@gmail.com>
2015-11-07 04:08:02 +0900

    Implement request.html data binding function

--- js/send_message.js
+++ js/send_message.js
@@ -11,8 +11,30 @@ $(function(){
         // ログインしていなかったらトップ画面へ移動
         window.location.href = 'toppage.html';
     }
-    
+
     var ticketID = window.common.getQueryString('ticket_id'); //URLのgetパラメータ取得
+
+    var vueObj = new Vue({
+        el:'#main',
+        data:{
+            ticketinfo:[]
+        }
+    });
+
+    // チケット情報の取得
+    $.ajax({
+		type: 'get',
+		url: 'http://210.140.71.3/tickets/'+ticketID+'.json',//' + ticketID + '.json',
+		contentType: 'application/x-www-form-urlencoded;application/json;application/json',
+		success: function(data){
+			console.log(data);
+			vueObj.ticketinfo = data;
+		},
+		error: function(xhr,textStatus,errorThrown){
+			console.log("auth error");
+			console.log(xhr,textStatus,errorThrown);
+		}
+	});
 
 	$("#send_ms_button").click(function(){
 		$.ajax({
@@ -21,7 +43,6 @@ $(function(){
 			contentType: 'application/x-www-form-urlencoded;application/json;application/json',
 			data: {"user":{ "token":localStorage.getItem("token")},"comment":$("#send_content").text()},
 			success: function(data){
-				alert(data);
 				location.href="./home.html";
 			},
 			error: function(xhr,textStatus,errorThrown){
