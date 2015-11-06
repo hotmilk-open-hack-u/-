d0076d097abb2a4f0f526528a5276025529521e7
UekusaTomoki <tmkueks19@gmail.com>
2015-11-06 16:44:29 +0900

    Implement teaching button function

--- js/detail2.js
+++ js/detail2.js
@@ -1,12 +1,15 @@
+var user_id;
+var user_token;
+var ticketID;
 $(function(){
     var applydata;
     var ticketdata;
 
     // ログイン情報の取得
     // user_id = localStorage.getItem("user_id");
-    user_id = 1;//test用
+    user_id = 2;//test用
     //user_token = localStorage.getItem("token");
-    user_token = "y8ZS4Vx8WXRHv2fe52KEMdYEybJwdFVK";//test用
+    user_token = "aeCZtZw2bKXo8bUCcmr_gZxWQWX4wDpT";//test用
     console.log("user_id:\t"+user_id+"\nuser_token:\t"+user_token);
 
     // ログイン状態の確認
@@ -17,7 +20,7 @@ $(function(){
     }
 
     // urlからパラメータの取得
-    var ticketID = window.common.getQueryString('ticket_id'); //URLのgetパラメータ取得
+    ticketID = window.common.getQueryString('ticket_id'); //URLのgetパラメータ取得
 
 	// データバインディングの設定
 	var vueApplyList = new Vue({
@@ -57,8 +60,25 @@ $(function(){
 			vueTickets.ticketinfo = data;
 		},
 		error: function(xhr,textStatus,errorThrown){
-			console.log("error.");
+			console.log("auth error");
 			console.log(xhr,textStatus,errorThrown);
 		}
 	});
 });
+// 教わるボタンが押された時の処理
+function teachRequest(id){
+    $.ajax({
+        method : 'POST',
+        url: "http://210.140.71.3/tickets/"+ticketID+"/select_teacher.json",
+        data:{"user":{"token":user_token},"teacher_id":id},
+        contentType: 'application/x-www-form-urlencoded;application/json;application/json',
+		success: function(data){
+		    console.log(data);
+            window.location.href = 'home.html';
+		},
+		error: function(xhr,textStatus,errorThrown){
+			console.log("error.");
+			console.log(xhr,textStatus,errorThrown);
+		}
+	});
+}
