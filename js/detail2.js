45030af8fee67749ecda074d0b386815b79c1120
UekusaTomoki <tmkueks19@gmail.com>
2015-11-06 15:14:13 +0900

    Implement other user's detail beta

---
+++ js/detail2.js
@@ -0,0 +1,64 @@
+$(function(){
+    var applydata;
+    var ticketdata;
+
+    // ログイン情報の取得
+    // user_id = localStorage.getItem("user_id");
+    user_id = 1;//test用
+    //user_token = localStorage.getItem("token");
+    user_token = "y8ZS4Vx8WXRHv2fe52KEMdYEybJwdFVK";//test用
+    console.log("user_id:\t"+user_id+"\nuser_token:\t"+user_token);
+
+    // ログイン状態の確認
+    if(user_id == null || user_token == null || user_id == undefined || user_token == undefined){
+        console.log("not login");
+        // ログインしていなかったらトップ画面へ移動
+        //window.location.href = 'toppage.html';
+    }
+
+    // urlからパラメータの取得
+    var ticketID = window.common.getQueryString('ticket_id'); //URLのgetパラメータ取得
+
+	// データバインディングの設定
+	var vueApplyList = new Vue({
+		el:'#applylist',
+		data:{
+			applylist:[]
+		}
+	});
+    var vueTickets = new Vue({
+		el:'#ticketpage',
+		data:{
+			ticketinfo:[],
+		}
+	});
+
+    // チケットの購入申請一覧を取得
+	$.ajax({
+		type: 'get',
+		url: 'http://210.140.71.3/tickets/'+ticketID+'/apply.json',//' + ticketID + '.json',
+		contentType: 'application/x-www-form-urlencoded;application/json;application/json',
+		success: function(data){
+			console.log(data);
+			vueApplyList.applylist = data.apply_list;
+		},
+		error: function(xhr,textStatus,errorThrown){
+			console.log("error.");
+			console.log(xhr,textStatus,errorThrown);
+		}
+	});
+    // チケット情報の取得
+    $.ajax({
+		type: 'get',
+		url: 'http://210.140.71.3/tickets/'+ticketID+'.json',//' + ticketID + '.json',
+		contentType: 'application/x-www-form-urlencoded;application/json;application/json',
+		success: function(data){
+			console.log(data);
+			vueTickets.ticketinfo = data;
+		},
+		error: function(xhr,textStatus,errorThrown){
+			console.log("error.");
+			console.log(xhr,textStatus,errorThrown);
+		}
+	});
+});
