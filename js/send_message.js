ebfdd7fb7479cb03258ae28dfc45dfa659661488
UekusaTomoki <tmkueks19@gmail.com>
2015-11-07 07:20:53 +0900

    Fixed bug

--- js/send_message.js
+++ js/send_message.js
@@ -36,14 +36,15 @@ $(function(){
 		}
 	});
 
-	$("#send_ms_button").click(function(){
+	$(".send_ms_button").click(function(){
 		$.ajax({
 			type: 'get',
 			url: 'http://210.140.71.3/tickets/' + ticketID + '/apply.json',
 			contentType: 'application/x-www-form-urlencoded;application/json;application/json',
 			data: {"user":{ "token":localStorage.getItem("token")},"comment":$("#send_content").text()},
 			success: function(data){
-				location.href="./home.html";
+                console.log(data);
+				//location.href="./home.html";
 			},
 			error: function(xhr,textStatus,errorThrown){
 				console.log("error.");
