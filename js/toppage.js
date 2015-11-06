1810a59a372f137bedba9b0b6c97167002811102
UekusaTomoki <tmkueks19@gmail.com>
2015-11-07 03:38:04 +0900

    Implement ticket template

--- js/toppage.js
+++ js/toppage.js
@@ -3,22 +3,22 @@ var offset = 0;
 $(document).ready(function(){
     // vueによってデータバインディング
     var vueTickets = new Vue({
-      el: '#content4-main',
+      el: '#content4-wrapper',
       data: {
         items: []
       }
     });
     // 注目の教えてを取得
     $.ajax({
-                type: 'GET',
-                url: 'http://210.140.71.3/tickets.json',
-                data: {"sort":"noticed","limit":5,"offset":offset},
-                dataType: "json",
-                success: function(data){
-                    vueTickets.items = data.tickets;
-                },
-                error : function(data) {
-                    console.log("error");
-                }
-            });
+        type: 'GET',
+        url: 'http://210.140.71.3/tickets.json',
+        data: {"sort":"noticed","limit":5,"offset":offset},
+        dataType: "json",
+        success: function(data){
+            vueTickets.items = data.tickets;
+        },
+        error : function(data) {
+            console.log("error");
+        }
+    });
 });
